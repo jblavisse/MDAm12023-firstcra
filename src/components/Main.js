1a8601d67a7a3f52d436ef176af6ea2064f762f3
Jb <jb@levillageducode.fr>
2023-02-01 13:34:27 +0100

    Set button with counter

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,20 @@ import {useState} from "react";
 
 function Main() {
     const [title, setTitle] = useState("Hello World!");
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setTitle("Olala, j'ai été changé");
+        setCount(count+1);
+    }
 
     return (
         <main>
             <h1>{title}</h1>
 
+            <button onClick={handleClick}>
+                Changer mon titre (déjà fait {count} fois)
+            </button>
             <ul>
                 <FoodItem title="Eau" quantity="10cl" />
                 <FoodItem title="Farine" quantity="30g" />
