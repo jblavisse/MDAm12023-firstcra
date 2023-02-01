9a615b4bcca4ce08c317abdd6f03b1f252929c76
Jb <jb@levillageducode.fr>
2023-02-01 13:48:08 +0100

    Set  Firstname with onchange event

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ import React, {useState} from 'react';
 function App() {
   const [firstname, setFirstname] = useState("Gertrude");
 
-  function handleClick() {
-    console.log("coucou");
-    setFirstname("Bruuuuuu");
+  function handleChange(event) {
+    console.log(event.target.value);
+    setFirstname(event.target.value);
   }
 
   return (
     <div className="App">
       <div className="container">
-        <Header/>
+        {/* <Header/>
         <Main/>
-        <Footer/>
+        <Footer/> */}
 
-        <p onClick={handleClick}>{firstname}</p>
+        <p>Prénom: {firstname}</p>
+
+        <label htmlFor="firstnameInput">Entrez votre prénom:</label>
+        <input type="text" id="firstnameInput"
+          value={firstname} onChange={handleChange} />
       </div>
     </div>
   );
